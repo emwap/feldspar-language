57c0245f601ce8e8098045e187777f65d07faa23
Anders Persson <anders.cj.persson@gmail.com>
2015-09-07 19:32:46 +0200

    Relax the type of twiddle factors

--- src/Feldspar/Algorithm/FFT/Twids.hs
+++ src/Feldspar/Algorithm/FFT/Twids.hs
@@ -5,12 +5,14 @@ import qualified Prelude
 import Feldspar
 import Feldspar.Vector
 
-twid :: Data Index -> Data Length -> Data Index -> Data (Complex Double)
+twid :: (Type a, Floating a, Prelude.RealFloat a)
+     => Data Index -> Data Length -> Data Index -> Data (Complex a)
 twid scale n k = share (1 / i2f n) $ \d -> cis (i2f scale * 2 * pi * i2f k * d)
 
-twids :: Data Length -> Pull1 (Complex Double)
+twids :: (Type a, Floating a, Prelude.RealFloat a)
+      => Data Length -> Pull1 (Complex a)
 twids n = indexed1 (n `div` 2) $ twid (-1) n
 
-itwids :: Data Length -> Pull1 (Complex Double)
+itwids :: (Type a, Floating a, Prelude.RealFloat a)
+       => Data Length -> Pull1 (Complex a)
 itwids n = indexed1 (n `div` 2) $ twid 1 n
-
